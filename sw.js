4ebac164b0d121f5682ff27e18bcc5392edf3d42
felipeloyolamejias-maker <felipe.loyolamejias@gmail.com>
2026-05-26 21:38:02 -0400

    Checklist V1

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE = 'flight-checklist-v6';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.23.2/babel.min.js',
+];
+
+function isAppDocument(request) {
+  if (request.mode === 'navigate') return true;
+  try {
+    const url = new URL(request.url);
+    return url.origin === self.location.origin &&
+      (url.pathname.endsWith('/') || url.pathname.endsWith('/index.html'));
+  } catch {
+    return false;
+  }
+}
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (isAppDocument(e.request)) {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const copy = res.clone();
+          caches.open(CACHE).then(cache => cache.put(e.request, copy));
+          return res;
+        })
+        .catch(() => caches.match(e.request).then(m => m || caches.match('./index.html')))
+    );
+    return;
+  }
+
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
